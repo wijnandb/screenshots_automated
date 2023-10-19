ec9a1b2380007ff6d8d58ff226cfabe3bc66017f
Wijnand Baretta <wijnandb@retta.nl>
2023-10-19 16:44:24 +0200

    Create get_links_per_role.py

---
+++ get_links_per_role.py
@@ -0,0 +1,30 @@
+from bs4 import BeautifulSoup
+import requests
+
+BASE_URL = 'https://yourwebsite.com'
+
+def get_all_links_for_role(role):
+    start_url = f"{BASE_URL}/{role}/"
+    visited = set()
+    to_visit = [start_url]
+
+    while to_visit:
+        url = to_visit.pop()
+        if url in visited:
+            continue
+        visited.add(url)
+
+        response = requests.get(url)
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        # Find all links within this page that starts with the role's directory.
+        for a_tag in soup.find_all('a', href=True):
+            href = a_tag['href']
+            if href.startswith(f"/{role}/"):
+                absolute_url = BASE_URL + href
+                to_visit.append(absolute_url)
+
+    return visited
+
+roles = ['parent', 'schoolmanager', 'teacher']
+all_pages = {role: get_all_links_for_role(role) for role in roles}
